0425936dbc5510d65c18d1a74a3e005259b5d044
mirkri <mirko.richter@uzh.ch>
2016-03-22 18:40:46 +0100

    Implement Positionable interface for Loot

--- src/main/java/ch/uzh/ifi/seal/soprafs16/model/Loot.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/Loot.java
@@ -4,18 +4,58 @@ package ch.uzh.ifi.seal.soprafs16.model;
  * Defines a loot on train or at player.
  * Created by mirkorichter on 22.03.16.
  */
-public class Loot {
-
+public class Loot implements Positionable{
+    
     /**
      * Defines type of this loot.
      */
     private LootType type;
-
+    
     /**
      * Defines value of this loot.
      */
     private int value;
-
+    
+    /**
+     * Train car level on which loot is.
+     */
+    private Positionable.Level level;
+    
+    /**
+     * Car on which loot is.
+     */
+    private int car;
+    
+    /**
+     * @return int The current car of the train for the positionable object.
+     */
+    public int getCar() {
+        return car;
+    }
+    
+    /**
+     * @return Level The level of the car for llot.
+     */
+    public Level getLevel() {
+        return level;
+    }
+    
+    /**
+     * Moves loot n cars in the direction of the locomotive.
+     * @param nrOfCarsToMove Number of cars to move.
+     */
+    public void moveToHead(int nrOfCarsToMove) {
+        car -= nrOfCarsToMove;
+    }
+    
+    /**
+     * Moves loot n cars in the direction of the trains tail.
+     * @param nrOfCarsToMove Number of cars to move.
+     */
+    public void moveToTail(int nrOfCarsToMove) {
+        car += nrOfCarsToMove;
+    }
+    
     /**
      * Sets type of this loot.
      * @param type type of this loot.
@@ -23,16 +63,16 @@ public class Loot {
     public void setType(LootType type) {
         this.type = type;
     }
-
+    
     /**
      * Returns type of loot.
      * @return type of loot.
      */
     public LootType getType() {
-
+        
         return type;
     }
-
+    
     /**
      * Sets value of this loot.
      * @param value value of this loot.
@@ -40,17 +80,17 @@ public class Loot {
     public void setValue(int value) {
         this.value = value;
     }
-
+    
     /**
      * Returns value of this loot.
      * @return value of this loot.
      */
     public int getValue() {
-
+        
         return value;
     }
-
-
-
-
+    
+    
+    
+    
 }
