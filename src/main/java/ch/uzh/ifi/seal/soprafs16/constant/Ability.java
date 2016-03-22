b1a823938158cf24867a85f3aaf142018ace4954
aah-mac <alexhofmann@gmail.com>
2016-03-22 19:06:42 +0100

    Added player class.
    Added player class and Ability enum.
    Player class contains the state of a playing character (not a marshal).

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/constant/Ability.java
@@ -0,0 +1,8 @@
+package ch.uzh.ifi.seal.soprafs16.constant;
+
+/**
+ * Created by alexanderhofmann on 22/03/16.
+ */
+public enum Ability {
+    GHOST, BELLE, CHEYENNE, TUCO, DJANGO, DOC, MARSHAL
+}
---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/Player.java
@@ -0,0 +1,99 @@
+package ch.uzh.ifi.seal.soprafs16.model;
+
+import ch.uzh.ifi.seal.soprafs16.constant.Ability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Defines the state of a playing character.
+ * Created by alexanderhofmann on 22/03/16.
+ */
+public class Player extends Meeple {
+
+    /**
+     * Bullets the player has left.
+     */
+    private int bullets;
+
+    /**
+     * List of loots.
+     */
+    private List<Loot> loots;
+
+    /**
+     * Player's special ability.
+     */
+    private Ability ability;
+
+    /**
+     * Default constructor.
+     */
+    public Player() {
+        this.loots = new ArrayList<>();
+    }
+
+    /**
+     * @param loot Loot to add to this player.
+     */
+    public Player(Loot loot) {
+        this();
+        loots.add(loot);
+    }
+
+    /**
+     * Gets the bullets the player has left.
+     * @return bullets left in chamber.
+     */
+    public int getBullets() {
+        return bullets;
+    }
+
+    /**
+     * Sets the number of bullets.
+     * @param bullets number of bullets to set.
+     */
+    public void setBullets(int bullets) {
+        this.bullets = bullets;
+    }
+
+    /**
+     * Gets a list of loots.
+     * @return
+     */
+    public List<Loot> getLoots() {
+        return loots;
+    }
+
+    /**
+     * Adds a piece of loot.
+     * @param loot Loot to pick.
+     */
+    public void addLoot(Loot loot) {
+        loots.add(loot);
+    }
+
+    /**
+     * Drops a piece of loot.
+     * @param loot Loot to drop.
+     */
+    public void dropLoot(Loot loot) {
+        loots.remove(loot);
+    }
+
+    /**
+     * Gets the player's special ability.
+     * @return player's ability.
+     */
+    public Ability getAbility() {
+        return ability;
+    }
+
+    /**
+     * Sets the player's special ability.
+     * @param ability Player's special ability.
+     */
+    public void setAbility(Ability ability) {
+        this.ability = ability;
+    }
+}
