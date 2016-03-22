2ad30313b00bc884c6957b44030fc14918266e53
mirkri <mirko.richter@uzh.ch>
2016-03-22 18:18:58 +0100

    Add comment

--- src/main/java/ch/uzh/ifi/seal/soprafs16/model/CardType.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/CardType.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.soprafs16.model;
 
 /**
+ * All possible type of card.
  * Created by mirkorichter on 22.03.16.
  */
 public enum CardType {
--- src/main/java/ch/uzh/ifi/seal/soprafs16/model/LootType.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/LootType.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.soprafs16.model;
 
 /**
+ * All possible types of a loot.
  * Created by mirkorichter on 22.03.16.
  */
 public enum LootType {
