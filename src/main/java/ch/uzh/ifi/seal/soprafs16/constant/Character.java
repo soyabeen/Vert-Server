ea5ca02093702540c224a58cc994be6e4e572d1e
aah-mac <alexhofmann@gmail.com>
2016-03-24 15:56:40 +0100

    Rename Player Ability to Player Character

--- src/main/java/ch/uzh/ifi/seal/soprafs16/constant/Character.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/constant/Character.java
@@ -3,6 +3,6 @@ package ch.uzh.ifi.seal.soprafs16.constant;
 /**
  * Created by alexanderhofmann on 22/03/16.
  */
-public enum Ability {
+public enum Character {
     GHOST, BELLE, CHEYENNE, TUCO, DJANGO, DOC, MARSHAL
 }
