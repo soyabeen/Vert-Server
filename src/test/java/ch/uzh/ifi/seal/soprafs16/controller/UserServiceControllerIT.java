39fc68b6568b495093d709d43a1dbf9fe134db49
aah-mac <alexhofmann@gmail.com>
2016-03-24 15:01:16 +0100

    Fixed unit test

--- src/test/java/ch/uzh/ifi/seal/soprafs16/controller/UserServiceControllerIT.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs16/controller/UserServiceControllerIT.java
@@ -46,20 +46,20 @@ public class UserServiceControllerIT {
     @Test
     @SuppressWarnings("unchecked")
     public void testCreateUserSuccess() {
-        List<User> usersBefore = template.getForObject(base + "/user", List.class);
+        List<User> usersBefore = template.getForObject(base + "/users", List.class);
         Assert.assertEquals(0, usersBefore.size());
 
         User request = new User("Mike Meyers", "mm");
 
         HttpEntity<User> httpEntity = new HttpEntity<User>(request);
 
-        ResponseEntity<User> response = template.exchange(base + "/user/", HttpMethod.POST, httpEntity, User.class);
+        ResponseEntity<User> response = template.exchange(base + "/users/", HttpMethod.POST, httpEntity, User.class);
         Assert.assertSame(1L, response.getBody().getId());
 
-        List<User> usersAfter = template.getForObject(base + "/user", List.class);
+        List<User> usersAfter = template.getForObject(base + "/users", List.class);
         Assert.assertEquals(1, usersAfter.size());
 
-        ResponseEntity<User> userResponseEntity = template.getForEntity(base + "/user/" + response.getBody().getId(), User.class);
+        ResponseEntity<User> userResponseEntity = template.getForEntity(base + "/users/" + response.getBody().getId(), User.class);
         User userResponse = userResponseEntity.getBody();
         Assert.assertEquals(request.getName(), userResponse.getName());
         Assert.assertEquals(request.getUsername(), userResponse.getUsername());
