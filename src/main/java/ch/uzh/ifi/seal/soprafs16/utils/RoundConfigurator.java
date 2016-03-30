f0de8ca603bbd9529485191c618a4cfbcadb6172
soyabeen <sein.anschluss@gmail.com>
2016-03-30 14:36:52 +0200

    Add log statements.

--- src/main/java/ch/uzh/ifi/seal/soprafs16/utils/RoundConfigurator.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/utils/RoundConfigurator.java
@@ -4,6 +4,8 @@ import ch.uzh.ifi.seal.soprafs16.constant.RoundEndEvent;
 import ch.uzh.ifi.seal.soprafs16.constant.Turn;
 import ch.uzh.ifi.seal.soprafs16.model.Game;
 import ch.uzh.ifi.seal.soprafs16.model.Round;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.naming.ConfigurationException;
 import java.util.*;
@@ -13,6 +15,7 @@ import java.util.*;
  */
 public class RoundConfigurator {
 
+    private static final Logger logger  = LoggerFactory.getLogger(RoundConfigurator.class);
     private static final int MAX_ROUNDS_FOR_GAME = 4;
 
     private List<RoundConfiguration> roundConfigs;
@@ -32,11 +35,13 @@ public class RoundConfigurator {
         roundConfigs.add(new RoundConfiguration(3, new Turn[]{Turn.NORMAL, Turn.HIDDEN, Turn.NORMAL, Turn.HIDDEN, Turn.NORMAL}, RoundEndEvent.NONE));
         roundConfigs.add(new RoundConfiguration(4, new Turn[]{Turn.NORMAL, Turn.NORMAL, Turn.HIDDEN, Turn.REVERSE}, RoundEndEvent.ANGRY_MARSHAL));
         roundConfigs.add(new RoundConfiguration(5, new Turn[]{Turn.NORMAL, Turn.NORMAL, Turn.NORMAL, Turn.NORMAL}, RoundEndEvent.BRAKING));
+        logger.debug("Added {0} RoundConfigurations for 2-4 players.", roundConfigs.size());
 
         stationConfigs = new ArrayList<>();
         stationConfigs.add(new RoundConfiguration(1, new Turn[]{Turn.NORMAL, Turn.NORMAL, Turn.HIDDEN, Turn.NORMAL}, RoundEndEvent.MARSHALS_REVENGE));
         stationConfigs.add(new RoundConfiguration(2, new Turn[]{Turn.NORMAL, Turn.NORMAL, Turn.HIDDEN, Turn.NORMAL}, RoundEndEvent.PICKPOCKETING));
         stationConfigs.add(new RoundConfiguration(3, new Turn[]{Turn.NORMAL, Turn.NORMAL, Turn.HIDDEN, Turn.NORMAL}, RoundEndEvent.HOSTAGE));
+        logger.debug("Added {0} RoundConfigurations for  players.", roundConfigs.size());
     }
 
     protected Round buildRoundWithConfig(Game game, RoundConfiguration config) {
@@ -46,7 +51,7 @@ public class RoundConfigurator {
     /**
      * Generate number (<code>MAX_ROUNDS_FOR_GAME</code> cards + one station card) of rounds for a game, using the
      * pre configured round definitions -> <code>ch.uzh.ifi.seal.soprafs16.utils.RoundConfigurator.RoundConfiguration</code>.
-     *
+     * @throws IllegalArgumentException if invalid round configurations exists.
      * @param game The game for the generated rounds.
      * @return List of generated round objects.
      */
@@ -56,16 +61,18 @@ public class RoundConfigurator {
         if (roundConfigs == null || MAX_ROUNDS_FOR_GAME > roundConfigs.size()) {
             throw new IllegalStateException("More rounds in MAX_ROUNDS_FOR_GAME than actual round configurations.");
         }
-
         if (stationConfigs == null || stationConfigs.size() == 0) {
             throw new IllegalStateException("No configurations for train station cards defined.");
         }
 
         Collections.shuffle(roundConfigs);
         Collections.shuffle(stationConfigs);
+
         for (int i = 0; i < MAX_ROUNDS_FOR_GAME; i++) {
+            logger.debug("Create round with round configuration id {0}", roundConfigs.get(i).getRcId());
             rounds.add(buildRoundWithConfig(game, roundConfigs.get(i)));
         }
+        logger.debug("Create train station with round configuration id {0}", stationConfigs.get(0).getRcId());
         rounds.add(buildRoundWithConfig(game, stationConfigs.get(0)));
 
         return rounds;
