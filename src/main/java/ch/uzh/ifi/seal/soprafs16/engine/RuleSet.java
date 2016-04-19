0381f4362a26ef4d08daa1488e1b121f17dbf429
soyabeen <sein.anschluss@gmail.com>
2016-04-19 18:46:52 +0200

    Introdce rule set

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/RuleSet.java
@@ -0,0 +1,11 @@
+package ch.uzh.ifi.seal.soprafs16.engine;
+
+/**
+ * Created by soyabeen on 19.04.16.
+ */
+public interface RuleSet {
+
+    public void emulate();
+    public void execute();
+
+}
