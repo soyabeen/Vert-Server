877fd615cf7ea58eacfaf8f41f6bb5437cfe476f
soyabeen <sein.anschluss@gmail.com>
2016-04-19 15:25:30 +0200

    Rem empty constructor.

--- src/main/java/ch/uzh/ifi/seal/soprafs16/exception/InvalidInputException.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/exception/InvalidInputException.java
@@ -7,9 +7,6 @@ package ch.uzh.ifi.seal.soprafs16.exception;
  */
 public class InvalidInputException extends IllegalArgumentException {
 
-    public InvalidInputException() {
-    }
-
     public InvalidInputException(String s) {
         super(s);
     }
