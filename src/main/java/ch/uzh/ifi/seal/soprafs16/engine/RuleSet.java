be9d6ed54b2a9ac37624f8b88f858e85209968e8
soyabeen <sein.anschluss@gmail.com>
2016-04-20 17:37:12 +0200

    Adjust to execution and simulation rule.

--- src/main/java/ch/uzh/ifi/seal/soprafs16/engine/RuleSet.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/RuleSet.java
@@ -1,11 +1,24 @@
 package ch.uzh.ifi.seal.soprafs16.engine;
 
+import ch.uzh.ifi.seal.soprafs16.model.Positionable;
+
+import java.util.List;
+
 /**
+ * Defines the set of rules needed for a certain card/activity to calculate the possibilities
+ * and to execute the actual chosen action.
+ * <p>
  * Created by soyabeen on 19.04.16.
  */
 public interface RuleSet {
 
-    public void emulate();
+    /**
+     * Calculates the possible target objects and outcomes for a card/activity.
+     *
+     * @return Possibilities The positionable objects that are possible targets for this card.
+     */
+    public List<Positionable> simulate();
+
     public void execute();
 
 }
