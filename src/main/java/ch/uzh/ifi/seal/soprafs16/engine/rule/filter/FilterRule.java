eb1e526207027cedf873953e6ae80886de2167e6
soyabeen <sein.anschluss@gmail.com>
2016-04-27 01:33:37 +0200

    Add comment

--- src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/filter/FilterRule.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/filter/FilterRule.java
@@ -5,6 +5,8 @@ import ch.uzh.ifi.seal.soprafs16.model.Positionable;
 import java.util.List;
 
 /**
+ * Filter rules take the given list of positionables and return a subset of that list.
+ * <p>
  * Created by soyabeen on 27.04.16.
  */
 public interface FilterRule {
@@ -12,4 +14,5 @@ public interface FilterRule {
     public boolean evaluate(List<Positionable> actors);
 
     public List<Positionable> filter(List<Positionable> actors);
+
 }
