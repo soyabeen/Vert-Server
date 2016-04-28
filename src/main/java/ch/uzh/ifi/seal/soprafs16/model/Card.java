4cd2cc8f82d73c8fab2ed7015218257fa4e36a40
mirkri <mirko.richter@uzh.ch>
2016-04-28 08:35:55 +0200

    add getter setter for id in card

--- src/main/java/ch/uzh/ifi/seal/soprafs16/model/Card.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/Card.java
@@ -107,4 +107,12 @@ public class Card implements Serializable {
                 "type=" + type +
                 '}';
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
