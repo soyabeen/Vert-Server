73abf524986e533f703907a77968d0acd735f6fd
soyabeen <sein.anschluss@gmail.com>
2016-04-30 02:15:40 +0200

    Rem unused imports.

--- src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/ShootRuleSet.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/ShootRuleSet.java
@@ -7,10 +7,7 @@ import ch.uzh.ifi.seal.soprafs16.engine.rule.sim.ShootPlayerSimRule;
 import ch.uzh.ifi.seal.soprafs16.model.Game;
 import ch.uzh.ifi.seal.soprafs16.model.Player;
 import ch.uzh.ifi.seal.soprafs16.model.Positionable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
