347b8c2f2ed1d22da29c17d19150b04d15c518f2
soyabeen <sein.anschluss@gmail.com>
2016-05-04 19:06:00 +0200

    Merge RoundCommand- and RoundQueryController to RoundController

--- src/main/java/ch/uzh/ifi/seal/soprafs16/controller/RoundController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/controller/RoundController.java
@@ -4,9 +4,8 @@ import ch.uzh.ifi.seal.soprafs16.constant.Turn;
 import ch.uzh.ifi.seal.soprafs16.dto.TurnDTO;
 import ch.uzh.ifi.seal.soprafs16.model.Move;
 import ch.uzh.ifi.seal.soprafs16.model.Round;
-import ch.uzh.ifi.seal.soprafs16.model.repositories.GameRepository;
-import ch.uzh.ifi.seal.soprafs16.service.PhaseLogicService;
 import ch.uzh.ifi.seal.soprafs16.service.RoundService;
+import org.hibernate.cfg.NotYetImplementedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +19,28 @@ import java.util.List;
  * Created by soyabeen on 26.03.16.
  */
 @RestController
-public class RoundQueryController extends GenericController {
+public class RoundController extends GenericController {
 
-    private static final Logger logger = LoggerFactory.getLogger(RoundQueryController.class);
+    private static final Logger logger = LoggerFactory.getLogger(RoundController.class);
 
     @Autowired
     private RoundService roundService;
 
-
     private final String CONTEXT = "/games/{gameId}/rounds";
+
+    @RequestMapping(value = CONTEXT + "/{nthRound}/turns", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public String makeMove(@PathVariable Long gameId, @PathVariable Integer nthRound, @RequestParam String token,
+                           @RequestBody TurnDTO turnDTO) {
+        Move move = roundService.getMoveFromDTO(gameId, token, turnDTO);
+        return roundService.makeAMove(gameId, nthRound, move);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    public TurnDTO executeMove(@PathVariable Long gameId, @PathVariable Integer nthRound, @RequestParam String token,
+                               @RequestBody TurnDTO turnDTO) {
+        throw new NotYetImplementedException("Method is missing");
+    }
 
     @RequestMapping(value = CONTEXT + "/{nthRound}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
