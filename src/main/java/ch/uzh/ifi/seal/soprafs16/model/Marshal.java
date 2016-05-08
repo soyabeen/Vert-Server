1bea36075222ea2b60a2cbabddbfa654f6d36aed
antonio galluccio <antonio.galluccio@gmail.com>
2016-05-08 18:14:40 +0200

    add Marshal wrapper

--- src/main/java/ch/uzh/ifi/seal/soprafs16/model/Marshal.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/Marshal.java
@@ -5,13 +5,9 @@ package ch.uzh.ifi.seal.soprafs16.model;
  */
 public class Marshal extends Meeple {
 
-    public Marshal() {}
-
     public Marshal(int car) {
         this.setCar(car);
+        this.setLevel(Level.BOTTOM);
     }
 
-    public void update(Marshal m) {
-        this.setCar(m.getCar());
-    }
 }
