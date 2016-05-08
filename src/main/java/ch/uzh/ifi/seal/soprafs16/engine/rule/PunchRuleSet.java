08e99104f5e818ddb30a578f9090145b18305801
antonio galluccio <antonio.galluccio@gmail.com>
2016-05-09 00:18:31 +0200

    add PunchRuleSet

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/PunchRuleSet.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.seal.soprafs16.engine.rule;
+
+import ch.uzh.ifi.seal.soprafs16.engine.ActionCommand;
+import ch.uzh.ifi.seal.soprafs16.model.Game;
+import ch.uzh.ifi.seal.soprafs16.model.Player;
+import ch.uzh.ifi.seal.soprafs16.model.Positionable;
+
+import java.util.List;
+
+/**
+ * Created by devuser on 09.05.2016.
+ */
+public class PunchRuleSet extends RuleSet {
+    @Override
+    public List<Positionable> simulate(Game game, Player player) {
+        return null;
+    }
+
+    @Override
+    public List<Positionable> execute(ActionCommand command) {
+        return null;
+    }
+}
