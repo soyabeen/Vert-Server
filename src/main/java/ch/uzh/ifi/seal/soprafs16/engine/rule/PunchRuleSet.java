34a088b242bc9db6f71a1023f7e51577dca7aafd
antonio galluccio <antonio.galluccio@gmail.com>
2016-05-11 20:58:57 +0200

    add Punch Rule

--- src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/PunchRuleSet.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/PunchRuleSet.java
@@ -2,25 +2,38 @@ package ch.uzh.ifi.seal.soprafs16.engine.rule;
 
 import ch.uzh.ifi.seal.soprafs16.engine.ActionCommand;
 import ch.uzh.ifi.seal.soprafs16.engine.rule.exec.PunchExecRule;
+import ch.uzh.ifi.seal.soprafs16.engine.rule.exec.MovePlayerExecRule;
+import ch.uzh.ifi.seal.soprafs16.engine.rule.filter.BelleNoTargetFilterRule;
+import ch.uzh.ifi.seal.soprafs16.engine.rule.replace.CheyennePunchRepRule;
+import ch.uzh.ifi.seal.soprafs16.engine.rule.replace.MarshalRepRule;
 import ch.uzh.ifi.seal.soprafs16.engine.rule.sim.PunchSimRule;
 import ch.uzh.ifi.seal.soprafs16.model.Game;
-import ch.uzh.ifi.seal.soprafs16.model.Player;
 import ch.uzh.ifi.seal.soprafs16.model.Positionable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by devuser on 09.05.2016.
  */
 public class PunchRuleSet extends RuleSet {
+
     @Override
     public List<Positionable> simulate(Game game, Positionable player) {
-        return new PunchSimRule(game).simulate(player);
+        PunchSimRule punch = new PunchSimRule(game);
+        BelleNoTargetFilterRule belle = new BelleNoTargetFilterRule();
+
+        return belle.filter(punch.simulate(player));
     }
 
     @Override
     public List<Positionable> execute(ActionCommand command) {
-        PunchExecRule punch = new PunchExecRule();
-        return punch.execute(command);
+        List<Positionable> result = new ArrayList<>();
+        PunchExecRule punchPlayer = new PunchExecRule();
+        MarshalRepRule marshalRepRule = new MarshalRepRule(command.getGame());
+        CheyennePunchRepRule cheyenne = new CheyennePunchRepRule(command);
+        
+        return marshalRepRule.replace(cheyenne.replace(punchPlayer.execute(command)));
     }
+
 }
