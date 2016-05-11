1fa8f6fc3a8943bdc918859ba4bd66045df83ad2
mirkri <mirko.richter@uzh.ch>
2016-05-11 18:23:34 +0200

    add getter for loots on train

--- src/main/java/ch/uzh/ifi/seal/soprafs16/dto/GameWithLastPlayedCardDTO.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/dto/GameWithLastPlayedCardDTO.java
@@ -4,8 +4,10 @@ import ch.uzh.ifi.seal.soprafs16.constant.GameStatus;
 import ch.uzh.ifi.seal.soprafs16.model.Card;
 import ch.uzh.ifi.seal.soprafs16.model.Loot;
 import ch.uzh.ifi.seal.soprafs16.model.Player;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -124,8 +126,19 @@ public class GameWithLastPlayedCardDTO implements Serializable {
         this.players = players;
     }
 
+    @JsonIgnore
     public List<Loot> getLoots() {
         return loots;
+    }
+
+    public List<Loot> getLootsOnTrain() {
+        List<Loot> lootsOnTrain = new ArrayList<>();
+        for(Loot l: loots) {
+            if (l.getOwnerId() == null) {
+                lootsOnTrain.add(l);
+            }
+        }
+        return lootsOnTrain;
     }
 
     public void setLoots(List<Loot> loots) {
