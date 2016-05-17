dfb2c8ca04edb8e287a0f3871c2373513e70b626
soyabeen <sein.anschluss@gmail.com>
2016-05-18 00:25:27 +0200

    Add test for unkown card type in game engine

--- src/test/java/ch/uzh/ifi/seal/soprafs16/engine/GameEngineIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs16/engine/GameEngineIntegrationTest.java
@@ -4,6 +4,7 @@ import ch.uzh.ifi.seal.soprafs16.constant.CardType;
 import ch.uzh.ifi.seal.soprafs16.helper.PositionedPlayer;
 import ch.uzh.ifi.seal.soprafs16.model.Game;
 import ch.uzh.ifi.seal.soprafs16.model.Player;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
@@ -26,5 +27,25 @@ public class GameEngineIntegrationTest {
         ActionCommand command = new ActionCommand(CardType.MOVE, game, actor, target);
         GameEngine engine = new GameEngine();
         engine.executeAction(command);
+    }
+
+    @Test
+    public void callEngineForUnknownCardType() throws InvocationTargetException {
+        Player actor = PositionedPlayer.builder().withUserName("actor").onUpperLevelAt(1).build();
+        Player target = PositionedPlayer.builder().withUserName("actor").onUpperLevelAt(0).build();
+
+        Game game = new Game();
+        game.setName("engine test game");
+        game.setNrOfCars(3);
+        game.addPlayer(actor);
+
+        ActionCommand command = new ActionCommand(CardType.BULLET, game, actor, target);
+        GameEngine engine = new GameEngine();
+        try {
+            engine.executeAction(command);
+            Assert.fail("Unknown card type should have forced an exception.");
+        }catch (IllegalArgumentException iae) {
+            Assert.assertTrue("Could not create RuleSet for unknown type.", iae.getMessage().contains("Could not create RuleSet"));
+        }
     }
 }
