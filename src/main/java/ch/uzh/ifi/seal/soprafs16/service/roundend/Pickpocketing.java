eea5da81a9321864b034fbfbb0d82f30f37ea8e4
antonio galluccio <antonio.galluccio@gmail.com>
2016-05-17 20:06:29 +0200

    add Pickpocketing EndEvent

--- src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/Pickpocketing.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/Pickpocketing.java
@@ -1,8 +1,11 @@
 package ch.uzh.ifi.seal.soprafs16.service.roundend;
 
 import ch.uzh.ifi.seal.soprafs16.model.Game;
+import ch.uzh.ifi.seal.soprafs16.model.Player;
 import ch.uzh.ifi.seal.soprafs16.model.Positionable;
+import ch.uzh.ifi.seal.soprafs16.utils.TargetFinder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +16,22 @@ public class Pickpocketing implements RoundEnd {
     @Override
     public List<Positionable> execute(Game game, List<Positionable> players) {
         // all players which are alone can pick up loot (if there is any)
-        return null;
+        List<Positionable> result = new ArrayList<>();
+
+        TargetFinder targetFinder = new TargetFinder();
+        List<Player> allPlayers = game.getPlayers();
+        List<Player> playersOnSameFloor = new ArrayList<>();
+
+        // iterate through all players and check if the player is alone
+        for(Player player : allPlayers) {
+            playersOnSameFloor.addAll( targetFinder.filterPlayersOnSameFloor(player, allPlayers) );
+
+            //if current player is alone
+            if(playersOnSameFloor.isEmpty()) {
+                result.add(player);
+            }
+        }
+
+        return result;
     }
 }
