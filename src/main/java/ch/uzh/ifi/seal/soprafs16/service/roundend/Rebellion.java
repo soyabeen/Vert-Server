7cf474c1b9924651cad16e36fe4af517305c808a
antonio galluccio <antonio.galluccio@gmail.com>
2016-05-17 20:22:43 +0200

    add Rebellion EndEvent

--- src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/Rebellion.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/Rebellion.java
@@ -1,8 +1,12 @@
 package ch.uzh.ifi.seal.soprafs16.service.roundend;
 
 import ch.uzh.ifi.seal.soprafs16.model.Game;
+import ch.uzh.ifi.seal.soprafs16.model.Marshal;
+import ch.uzh.ifi.seal.soprafs16.model.Player;
 import ch.uzh.ifi.seal.soprafs16.model.Positionable;
+import ch.uzh.ifi.seal.soprafs16.utils.TargetFinder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +17,19 @@ public class Rebellion implements RoundEnd {
     @Override
     public List<Positionable> execute(Game game, List<Positionable> players) {
         // all players inside the train get a bullet card
-        return null;
+        List<Positionable> result = new ArrayList<>();
+
+        TargetFinder targetFinder = new TargetFinder();
+        Marshal marshal = new Marshal(game.getPositionMarshal());
+        List<Player> playersInsideTrain = new ArrayList<>();
+        // all players on same floor as marshal (= inside of train)
+        playersInsideTrain.addAll( targetFinder.filterPlayersOnSameFloor(marshal, game.getPlayers()) );
+
+        for(Player target : playersInsideTrain) {
+            target.getsShot();
+            result.add(target);
+        }
+
+        return result;
     }
 }
