fcd0266318df7c05cf444a5c6d17d4bf03c2a527
antonio galluccio <antonio.galluccio@gmail.com>
2016-05-17 19:38:41 +0200

    add Hostage EndEvent

--- src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/Hostage.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/Hostage.java
@@ -1,8 +1,12 @@
 package ch.uzh.ifi.seal.soprafs16.service.roundend;
 
+import ch.uzh.ifi.seal.soprafs16.constant.LootType;
 import ch.uzh.ifi.seal.soprafs16.model.Game;
+import ch.uzh.ifi.seal.soprafs16.model.Loot;
+import ch.uzh.ifi.seal.soprafs16.model.Player;
 import ch.uzh.ifi.seal.soprafs16.model.Positionable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +17,14 @@ public class Hostage implements RoundEnd {
     @Override
     public List<Positionable> execute(Game game, List<Positionable> players) {
         // all players get 250$ reward
-        return null;
+        List<Positionable> result = new ArrayList<>();
+
+        List<Player> allPlayers = game.getPlayers();
+        for(Player player : allPlayers) {
+            player.addLoot(new Loot(LootType.PURSE_SMALL, game.getId(), 250));
+            result.add(player);
+        }
+
+        return result;
     }
 }
