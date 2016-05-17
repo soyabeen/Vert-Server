497f9aebde4d123a1f97de92c440ece9603f486a
soyabeen <sein.anschluss@gmail.com>
2016-05-18 00:50:13 +0200

    Add django move rep rule

--- src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/ShootRuleSet.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/ShootRuleSet.java
@@ -4,6 +4,7 @@ import ch.uzh.ifi.seal.soprafs16.engine.ActionCommand;
 import ch.uzh.ifi.seal.soprafs16.engine.rule.exec.ShootExecRule;
 import ch.uzh.ifi.seal.soprafs16.engine.rule.filter.BelleNoTargetFilterRule;
 import ch.uzh.ifi.seal.soprafs16.engine.rule.filter.TucoAdditionalTargetFilterRule;
+import ch.uzh.ifi.seal.soprafs16.engine.rule.replace.DjangoMoveRepRule;
 import ch.uzh.ifi.seal.soprafs16.engine.rule.sim.ShootPlayerSimRule;
 import ch.uzh.ifi.seal.soprafs16.model.Game;
 import ch.uzh.ifi.seal.soprafs16.model.Player;
@@ -21,13 +22,13 @@ public class ShootRuleSet extends RuleSet {
         ShootPlayerSimRule shoot = new ShootPlayerSimRule(game);
         BelleNoTargetFilterRule belle = new BelleNoTargetFilterRule();
         TucoAdditionalTargetFilterRule tuco = new TucoAdditionalTargetFilterRule(game, (Player) player);
-
         return belle.filter(tuco.filter(shoot.simulate(player)));
     }
 
     @Override
     public List<Positionable> execute(ActionCommand command) {
         ShootExecRule shoot = new ShootExecRule();
-        return shoot.execute(command);
+        DjangoMoveRepRule django = new DjangoMoveRepRule(command);
+        return django.replace(shoot.execute(command));
     }
 }
---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/engine/rule/replace/DjangoMoveRepRule.java
@@ -0,0 +1,82 @@
+package ch.uzh.ifi.seal.soprafs16.engine.rule.replace;
+
+import ch.uzh.ifi.seal.soprafs16.constant.CardType;
+import ch.uzh.ifi.seal.soprafs16.constant.Character;
+import ch.uzh.ifi.seal.soprafs16.constant.Direction;
+import ch.uzh.ifi.seal.soprafs16.engine.ActionCommand;
+import ch.uzh.ifi.seal.soprafs16.engine.rule.exec.MovePlayerExecRule;
+import ch.uzh.ifi.seal.soprafs16.model.Player;
+import ch.uzh.ifi.seal.soprafs16.model.Positionable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by soyabeen on 15.05.16.
+ */
+public class DjangoMoveRepRule implements ReplaceRule {
+
+    private ActionCommand command;
+
+    public DjangoMoveRepRule(ActionCommand command) {
+        this.command = command;
+    }
+
+    private Player filterDjango(List<Positionable> players) {
+        for (Positionable pos : players) {
+            if (pos instanceof Player) {
+                Player p = (Player) pos;
+                if (Character.DJANGO.equals(p.getCharacter())) {
+                    return p;
+                }
+            }
+        }
+        return null;
+    }
+
+    private Player filterById(Player player, List<Positionable> players) {
+        for (Positionable pos : players) {
+            if (pos instanceof Player) {
+                Player p = (Player) pos;
+                if (player.getId().equals(p.getId())) {
+                    return p;
+                }
+            }
+        }
+        return null;
+    }
+
+    private Direction getDirectionToMove(Player source, Player target) {
+        int raw = source.getCar() - target.getCar();
+        if (raw < 0) {
+            return Direction.TO_HEAD;
+        }
+        return Direction.TO_TAIL;
+    }
+
+    @Override
+    public boolean evaluate(List<Positionable> actors) {
+        return Character.DJANGO.equals(((Player) command.getCurrentPlayer()).getCharacter())
+                && filterDjango(actors) != null
+                && actors.size() >= 2;
+    }
+
+    @Override
+    public List<Positionable> replace(List<Positionable> actors) {
+        List<Positionable> result = new ArrayList<>();
+        if (evaluate(actors)) {
+            Player django = filterById((Player) command.getCurrentPlayer(), actors);
+            Player target = filterById((Player) command.getTargetPlayer(), actors);
+
+            Direction dirToMove = getDirectionToMove(django, target);
+            Player newTargetPosition = new Player();
+            newTargetPosition.setLevel(target.getLevel());
+            newTargetPosition.setCar(target.getCar() + dirToMove.intValue());
+
+            ActionCommand moveCommand = new ActionCommand(CardType.MOVE, command.getGame(), target, newTargetPosition);
+            result.addAll(new MovePlayerExecRule().execute(moveCommand));
+            result.add(django);
+        }
+        return result;
+    }
+}
