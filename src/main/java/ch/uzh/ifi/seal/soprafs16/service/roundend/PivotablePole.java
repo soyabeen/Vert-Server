9e164b61b9cafedbd2d69ec2f40740e31a21fe0e
antonio galluccio <antonio.galluccio@gmail.com>
2016-05-17 20:13:31 +0200

    add PivotablePole EndEvent

--- src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/PivotablePole.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/service/roundend/PivotablePole.java
@@ -1,8 +1,11 @@
 package ch.uzh.ifi.seal.soprafs16.service.roundend;
 
 import ch.uzh.ifi.seal.soprafs16.model.Game;
+import ch.uzh.ifi.seal.soprafs16.model.Marshal;
 import ch.uzh.ifi.seal.soprafs16.model.Positionable;
+import ch.uzh.ifi.seal.soprafs16.utils.TargetFinder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +16,19 @@ public class PivotablePole implements RoundEnd {
     @Override
     public List<Positionable> execute(Game game, List<Positionable> players) {
         // all players on the roof will be positioned on last waggon
-        return null;
+        List<Positionable> result = new ArrayList<>();
+
+        TargetFinder targetFinder = new TargetFinder();
+        Marshal marshal = new Marshal(game.getPositionMarshal());
+        List<Positionable> affectedPlayers = new ArrayList<>();
+        // all players on oppsite floor of marshal (=roof)
+        affectedPlayers.addAll( targetFinder.filterPlayersOnOppositeFloor(marshal, game.getPlayers()) );
+
+        for(Positionable player : affectedPlayers) {
+            player.setCar(game.getNrOfCars());
+            result.add(player);
+        }
+
+        return result;
     }
 }
